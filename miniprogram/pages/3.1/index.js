419afa2383e532fff0b8aba0f730df8f478aaae2
jay.jia <jay.jia@livenaked.com>
2021-01-17 22:54:54 +0800

    new pages 3.1-other

--- miniprogram/pages/3.1/index.js
+++ miniprogram/pages/3.1/index.js
@@ -10,6 +10,78 @@ Page({
   },
 
   /**
+   * 3.1 测试一个网络请求，及返回
+   */
+  startOneRequest: function(){
+    // 正常
+    wx.request({
+      url: 'http://localhost:3009/hi',
+      success(res){
+        if (res.errMsg === "request:ok") {
+          console.log("res1", res);
+        }
+      },
+      fail(err){
+        if (err.errMsg === "request:fail") {
+          console.log("err1", err);
+        }
+      },
+      complete(resOrErr){
+        console.log("resOrErr1", resOrErr);
+      }
+    });
+
+    // 错误
+    wx.request({
+      url: 'http://localhost:3009/err',
+      success(res){
+        if (res.errMsg === "request:ok") {
+          console.log("res2", res);
+        }
+      },
+      fail(err){
+        if (err.errMsg === "request:fail") {
+          console.log("err2", err);
+        }
+      },
+      complete(resOrErr){
+        console.log("resOrErr2", resOrErr);
+      }
+    });
+
+    // 取消
+    let reqTask = wx.request({
+      url: 'http://localhost:3009/err',
+      success(res){
+        if (res.errMsg === "request:ok") {
+          console.log("res3", res);
+        }
+      },
+      fail(err){
+        if (err.errMsg === "request:fail") {
+          console.log("err3", err);
+        }
+      },
+      complete(resOrErr){
+        // 被取消时，也会被调用
+        console.log("resOrErr3", resOrErr);
+      }
+    });
+    const headersReceivedCallback = function(headers){
+      // "use strict"
+      reqTask.offHeadersReceived(headersReceivedCallback);
+      console.log('headers', headers);
+      // Procted resource = 18 chars
+      // 能拿到这个长度，可能数据已经返回了，可以基于其他逻辑实施abort
+      if (~~headers.header['Content-Length'] < 19) {
+        reqTask.abort();
+      }
+      reqTask.onHeadersReceived(headersReceivedCallback);
+      // reqTask.abort()
+    }
+  },
+
+  /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
@@ -63,64 +135,5 @@ Page({
    */
   onShareAppMessage: function () {
 
-  },
-  /**
-   * 3.1 测试一个网络请求，及返回
-   */
-  startOneRequest: function(){
-    // 正常
-    wx.request({
-      url: 'http://localhost:3000/hi',
-      success(res){
-        if (res.errMsg === "request:ok") {
-          console.log("res1", res);
-        }
-      },
-      fail(err){
-        if (err.errMsg === "request:fail") {
-          console.log("err1", err);
-        }
-      },
-      complete(resOrErr){
-        console.log("resOrErr1", resOrErr);
-      }
-    });
-
-    // 错误
-    wx.request({
-      url: 'http://localhost:3000/err',
-      success(res){
-        if (res.errMsg === "request:ok") {
-          console.log("res2", res);
-        }
-      },
-      fail(err){
-        if (err.errMsg === "request:fail") {
-          console.log("err2", err);
-        }
-      },
-      complete(resOrErr){
-        console.log("resOrErr2", resOrErr);
-      }
-    });
-
-    // 取消
-    let reqTask = wx.request({
-      url: 'http://localhost:3000/err',
-      success(res){
-        if (res.errMsg === "request:ok") {
-          console.log("res3", res);
-        }
-      },
-      fail(err){
-        if (err.errMsg === "request:fail") {
-          console.log("err3", err);
-        }
-      },
-      complete(resOrErr){
-        // 被取消时，也会被调用
-        console.log("resOrErr3", resOrErr);
-      }
-    });
   }
 })
