e9ed717d927d9bc122cda633479f15a96a47420b
jay <jay.jia@livenaked.com>
2021-01-27 18:20:01 +0800

    upgrade page category

--- miniprogram/pages/category/index.js
+++ miniprogram/pages/category/index.js
@@ -28,8 +28,9 @@ Page({
     for (let j = 0; j < categories.length; j++) {
       let item = categories[j];
       if (j<3) {
-        // 
+        this.getGoodsListByCategory(item.id, j);
       }
+      this.getGoodsListByCategory(item.id);
       vtabs.push({title: item.category_name, id: item.id});
     }
 
@@ -87,9 +88,90 @@ Page({
   onShareAppMessage: function () {
 
   },
+  /**
+   * 重新计算高度
+   * @param {*} index 
+   */
+  reClacChildHeight(index) {
+    // calcChildHeight
+    const goodsContent = this.selectComponent(`#goods-content${index}`);
+    console.log(goodsContent);
+
+    const categoryVtabs = this.selectComponent('#category-vtabs');
+    // categoryVtabs.calcChildHeight(goodsContent);
+  },
   async getGoodsListByCategory(categoryId, index, loadNextPage = false){
+    console.log(categoryId, index, loadNextPage);
+
     const pageSize = 10;
     let pageIndex = 1;
     let listMap = this.data.goodsListMap[categoryId];
+    console.log(listMap);
+    
+    if (listMap) {
+      console.log(listMap.count);
+      // 加载完了，就不要重复加载了
+      if (listMap.rows.length >= listMap.count) {
+        return;
+      }
+      if (listMap.pageIndex) {
+        pageIndex = listMap.pageIndex;
+        if (loadNextPage) {
+          pageIndex++;
+        }
+      }
+    }
+    let goodsData = await wx.wxp.request({
+      url: `http://localhost:3009/goods/goods?page_index=${pageIndex}&page_size=${pageSize}&category_id=${categoryId}`,
+    });
+    if (goodsData) {
+      goodsData = goodsData.data.data;
+    }
+    console.log(goodsData);
+    if (listMap) {
+      listMap.pageIndex = pageIndex;
+      listMap.count = goodsData.count;
+      listMap.rows.push(...goodsData.rows);
+      console.log(listMap);
+      this.setData({
+        [`goodsListMap[${categoryId}]`]:listMap
+      });
+    } else {
+      goodsData.pageIndex = pageIndex;
+      this.setData({
+        [`goodsListMap[${categoryId}]`]:goodsData
+      });
+    }
+
+    this.data.goodsListMap[categoryId] = goodsData;
+    this.reClacChildHeight(index);
+  },
+  onScrollToIndexLower(e){
+    console.log("scroll to index lower", e.detail);
+    let index = e.detail.index;
+    // 这是一个多发事件
+    if (index != this.data.lastIndexForLoadMore) {
+      let cate = this.data.vtabs[index];
+      let categoryId = cate.id;
+      this.getGoodsListByCategory(categoryId, index, true);
+      this.data.lastIndexForLoadMore = index;
+    }
+  },
+  onCategoryChanged(index){
+    let cate = this.data.vtabs[index];
+    let category_id = cate.id;
+    if (!this.data.goodsListMap[category_id]) {
+      this.getGoodsListByCategory(category_id, index);
+    }
+  },
+  onTabCLick(e){
+    const index = e.detail.index;
+    console.log('tabClick', index);
+    this.onCategoryChanged(index);
+  },
+  onChange(e){
+    const index = e.detail.index;
+    console.log('change', index);
+    this.onCategoryChanged(index);
   }
 })
