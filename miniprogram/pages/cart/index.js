06a1c7f3a33ad4e85c49d0d7bbe67d5b3e9b2cdd
jay <jay.jia@livenaked.com>
2021-02-02 16:47:46 +0800

    upgrade page-cart

--- miniprogram/pages/cart/index.js
+++ miniprogram/pages/cart/index.js
@@ -88,7 +88,26 @@ Page({
   onShareAppMessage: function () {
 
   },
-  calcTotalPrice(){},
+  /**
+   * 重新计算总价
+   */
+  calcTotalPrice(){
+    let totalPrice = 0;
+    let ids = this.data.cartIdSelectedResult;
+    let carts = this.data.carts;
+    ids.forEach(id=>{
+      carts.some(item=>{
+        if (item.id == id) {
+          totalPrice += item.price * item.num;
+          return true;
+        }
+        return false;
+      });
+    });
+    this.setData({
+      totalPrice
+    });
+  },
   changeEditMode(){
     let editMode = !this.data.editMode;
     this.setData({
@@ -104,15 +123,110 @@ Page({
     this.calcTotalPrice();
   },
   onSelectAll(event){
+    let allIsSelected = event.detail;
+    let cartIdSelectedResult = this.data.cartIdSelectedResult;
+    cartIdSelectedResult.length = 0;
 
+    if (allIsSelected) {
+      let carts = this.data.carts;
+      for (let j = 0; j < carts.length; j++) {
+        cartIdSelectedResult.push(`${carts[j].id}`);
+      }
+    }
+
+    this.setData({
+      allIsSelected,
+      cartIdSelectedResult
+    });
+    this.calcTotalPrice();
   },
   onCartConfirm(e){
     // 拿到列表数据
+    let carts = this.data.carts;
+    let cartData = []
+    let ids = this.data.cartIdSelectedResult;
+    if (ids.length == 0) {
+      wx.showModal({
+        title: '为选择商品',
+        showCancel: false
+      })
+      return
+    }
+    ids.forEach(id=>{
+      carts.some(item=>{
+        if (item.id == id) {
+          cartData.push(Object.assign({}, item));
+          return true;
+        } 
+        return false;
+      });
+    });
+    wx.navigateTo({
+      url: '/pages/confirm-order/index',
+      success: function(res) {
+        res.eventChannel.emit('cartData', {data: cartData})
+      }
+    })
   },
   async onCartGoodsNumChanged(e) {
-    
+    let cartGoodsId = e.currentTarget.dataset.id;
+    let oldNum = parseInt(e.currentTarget.dataset.num);
+    console.log('e.detail', typeof e.detail, cartGoodsId, oldNum);
+    // console.log(e.detail);
+    let num = e.detail;
+    let data = {num}
+
+    let res = await getApp().wxp.request4({
+      url:`http://localhost:3009/user/my/carts/${cartGoodsId}`,
+      method:'put',
+      data
+    });
+    if (res.data.msg == 'ok') {
+      wx.showToast({
+        title: num > oldNum ? '增加成功' : '减少成功',
+      })
+      // 修复数据
+      let carts = this.data.carts;
+      carts.some(item=>{
+        if (item.id == cartGoodsId) {
+          item.num = num
+          return true;
+        }
+        return false;
+      });
+      this.calcTotalPrice();
+    }
   },
   async removeCartGoods(e){
-
+    let ids = this.data.cartIdSelectedResult;
+    if (ids.length == 0) {
+      wx.showModal({
+        title: '为选择商品',
+        showCancel: false
+      })
+      return
+    }
+    let data = {ids}
+    let res = await getApp().wxp.request4({
+      url: 'http://localhost:3009/user/my/carts',
+      method: 'delete',
+      data
+    });
+    if (res.data.msg == 'ok') {
+      let carts = this.data.carts;
+      for (let j = 0; j < ids.length; j++) {
+        let id = ids[j];
+        carts.some((item,index)=>{
+          if (item.id == id) {
+            carts.splice(index,1);
+            return true
+          }
+          return false
+        });
+      }
+      this.setData({
+        carts
+      });
+    }
   }
 })
