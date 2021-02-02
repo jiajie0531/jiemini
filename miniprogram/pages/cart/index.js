026b7991058996d4d6934427201e62eb73e42573
jay <jay.jia@livenaked.com>
2021-02-02 15:29:39 +0800

    upgrade page-cart

--- miniprogram/pages/cart/index.js
+++ miniprogram/pages/cart/index.js
@@ -16,8 +16,18 @@ Page({
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
-
+  onLoad: async function (options) {
+    // let res = await getApp().wxp.request4({
+    //   url: 'http://localhost:3009/user/my/carts',
+    //   method: 'get'
+    // });
+    // console.log(res);
+    // if (res.data.msg == 'ok') {
+    //   let carts = res.data.data;
+    //   this.setData({
+    //     carts
+    //   });
+    // }
   },
 
   /**
@@ -30,8 +40,18 @@ Page({
   /**
    * Lifecycle function--Called when page show
    */
-  onShow: function () {
-
+  onShow: async function () {
+    let res = await getApp().wxp.request4({
+      url: 'http://localhost:3009/user/my/carts',
+      method: 'get'
+    });
+    console.log(res);
+    if (res.data.msg == 'ok') {
+      let carts = res.data.data;
+      this.setData({
+        carts
+      });
+    }
   },
 
   /**
@@ -66,6 +86,33 @@ Page({
    * Called when user click on the top right corner to share
    */
   onShareAppMessage: function () {
+
+  },
+  calcTotalPrice(){},
+  changeEditMode(){
+    let editMode = !this.data.editMode;
+    this.setData({
+      editMode
+    });
+  },
+  onSelectGoodsItem(e){
+    // console.log(e.detail);
+    let cartIdSelectedResult = e.detail;
+    this.setData({
+      cartIdSelectedResult
+    });
+    this.calcTotalPrice();
+  },
+  onSelectAll(event){
+
+  },
+  onCartConfirm(e){
+    // 拿到列表数据
+  },
+  async onCartGoodsNumChanged(e) {
+    
+  },
+  async removeCartGoods(e){
 
   }
 })
