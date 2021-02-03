4ade64687b43cd342dda856b4f862f9af68a17f1
jay <jay.jia@livenaked.com>
2021-02-03 15:23:35 +0800

    upgrade-page new-address

--- miniprogram/pages/new-address/index.js
+++ miniprogram/pages/new-address/index.js
@@ -76,7 +76,56 @@ Page({
   onShareAppMessage: function () {
 
   },
-  async save(e){},
+  async save(e){
+    // let data = this.data;
+    // console.log(data);
+    let userName = this.data.userName;
+    let telNumber = this.data.telNumber;
+    let detailInfo = this.data.detailInfo;
+    let region = this.data.region;
+    let id = this.data.id;
+
+    if (!userName || !telNumber || !detailInfo) {
+      wx.showModal({
+        title: '数据项不能为空',
+      })
+      return
+    }
+    if (!/[\d-]{11,18}/.test(telNumber)){
+      wx.showModal({
+        title: '电话格式对吗？',
+      })
+      return
+    }
+    let data = {
+      userName,
+      telNumber,
+      detailInfo,
+      region,
+      id
+    }
+    let method = id ? 'put' : 'post';
+    let res = await wx.wxp.request4({
+      url: 'http://localhost:3009/user/my/address',
+      method,
+      data
+    });
+    if (res.data.msg == 'ok') {
+      let opener = this.getOpenerEventChannel();
+      let address = this.data
+      if (!id) {
+        address.id = res.data.data.id;
+      }
+      opener.emit("savedNewAddress", address);
+      wx.navigateBack({
+        delta: 1,
+      })
+    } else {
+      wx.showModal({
+        title: '添加失败，是否电话重复了？',
+      })
+    }
+  },
   bindRegionChange: function (e) {
     this.setData({
       region: e.detail.value
