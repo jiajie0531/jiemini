84e2fa8e8a882085991f0e8c9efa519aea4adc16
jay <jay.jia@livenaked.com>
2021-02-04 11:00:03 +0800

    upgrade page confirm-order

--- miniprogram/pages/confirm-order/index.js
+++ miniprogram/pages/confirm-order/index.js
@@ -5,14 +5,29 @@ Page({
    * Page initial data
    */
   data: {
-
+    carts:[],
+    userMessage:'',
+    totalPrice:0,
+    address:{
+      userName:'选择'
+    },
+    submchPayParams: {}, 
+    submchPayorderResult:{},
+    prepareSubmchPay: false
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-
+    const eventChannel = this.getOpenerEventChannel();
+    eventChannel.on('cartData', (res) => {
+      console.log(res);
+      this.setData({
+        carts:res.data
+      });
+      this.calcTotalPrice();
+    });
   },
 
   /**
@@ -62,5 +77,53 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  onSubmit(e){
+    wx.showActionSheet({
+      itemList: ['默认支付', '小微商户'],
+      success:(res)=>{
+        console.log(res.tapIndex);
+        let index = res.tapIndex;
+        if (index == 0) { // 默认支付
+          this.startNormalPay(e);
+        } else if (index == 1) { // 小微商户
+          this.startSubmchPay(e)
+        }
+      },
+      fail(res){
+        console.log(res.errMsg)
+      }
+    });
+  },
+  async startNormalPay(e){},
+  async startSubmchPay(e){},
+  /**
+   * 准备跳转到地址列表，选取地址
+   */
+  toSelectAddress(){
+    wx.navigateTo({
+      url: '/pages/address-list/index',
+      success:res=>{
+        res.eventChannel.on('selectAddress', address=>{
+          address.addressInfo = address.region.join('')+address.detailInfo;
+          this.setData({
+            address
+          });
+        });
+      }
+    });
+  },
+  /**
+   * 重新计算总价
+   */
+  calcTotalPrice(){
+    let totalPrice = 0;
+    let carts = this.data.carts;
+    carts.forEach(item=>{
+      totalPrice += item.price * item.num
+    });
+    this.setData({
+      totalPrice
+    });
   }
 })
